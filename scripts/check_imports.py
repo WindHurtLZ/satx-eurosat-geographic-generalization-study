26ed0d039ea25c98357acf4cc61e7617bc148164
Xufan Lin <xlin388@gatech.edu>
2026-07-08 00:50:59 -0400

    infra: project bootstrap

---
+++ scripts/check_imports.py
@@ -0,0 +1,9 @@
+import satx
+import satx.data
+import satx.models
+import satx.engine
+import satx.eval
+import satx.analysis
+import satx.utils
+
+print("All SatX imports passed.")
---
+++ tests/test_imports.py
@@ -0,0 +1,11 @@
+def test_import_satx():
+    import satx
+
+
+def test_import_subpackages():
+    import satx.data
+    import satx.models
+    import satx.engine
+    import satx.eval
+    import satx.analysis
+    import satx.utils
